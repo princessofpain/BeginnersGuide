76367f4654200d0c99f9e8a2fdb099fd501b5826
Chilja Speransky <magiclanzelot@gmx.net>
2018-08-14 14:29:00 +0200

    Unit tests added

--- test/test/ByteInputTest.java
+++ test/test/ByteInputTest.java
@@ -1,14 +1,64 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import usingIO.ByteInput;
 
 class ByteInputTest {
+	
+	private static ByteInput input = new ByteInput();
+	private static ByteInputTest inputTest = new ByteInputTest();
 
 	@Test
-	void test() {
-
+	void lengthIsReturned() {
+		int expectedResult = 51;
+		int actualResult = input.getLengthOfInputStream();
+		
+		assertEquals(expectedResult, actualResult);
 	}
 
-}
+	@Test 
+	void readsOneByte() {
+		int expectedResult = 73;
+		int actualResult = input.readOneByteOfInputStream();
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test 
+	void readsAllBytes() {
+		Byte[] actualResult = input.readAllBytesOfInputStream();
+		Byte[] expectedResult = {73, 32, 97, 109, 32, 97, 32, 116, 101, 115, 116, 32, 102, 105, 108, 101, 46, 32, 13, 10, 85, 115, 101, 32, 109, 101, 32, 116, 111, 32, 116, 101, 115, 116, 32, 116, 104, 101, 32, 73, 47, 79, 32, 111, 102, 32, 74, 97, 118, 97, 46};
+
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void convertsBytesToChars() {	
+		
+		Byte[] byteInput = input.readAllBytesOfInputStream();
+		
+		String expectedResult = "I am a test file. Use me to test the I/O of Java.";
+
+		char[] allChars = input.convertBytesToChars(byteInput);
+		String actualResult = ByteInputTest.convertCharsToString(allChars); 
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	private static String convertCharsToString(char[] allChars) {
+		StringBuilder str = new StringBuilder();
+		
+		for(char c: allChars) {
+			if(c > 31) {
+				str.append(c);
+			}
+		}
+		
+		return str.toString();
+	}
+}	
+
